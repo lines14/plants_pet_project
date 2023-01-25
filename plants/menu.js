d251f9a16a527c6f28011a44942bfcc753840b79
Anton Lipatov <tonybass1414@yahoo.com>
2023-01-26 01:53:01 +0600

    feat: finally implement adaptive js menu

--- plants/menu.js
+++ plants/menu.js
@@ -1,6 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector("header").classList.toggle("open")
+const burgerButton = document.querySelector('.burger-menu');
+
+if (burgerButton) {
+    
+   const navList = document.querySelector('.nav-list');
+   const navItems = document.querySelectorAll('.nav-item');
+   const burgerBars = document.querySelectorAll('.bar');
+   const main = document.querySelector('.main');
+   const footer = document.querySelector('.footer');
+
+   navList.addEventListener('click', (event) => {
+        const clickedItem = findParent(event.target, 'nav-item');
+        if (!clickedItem) {
+            menuClickHandler();
+        };
     })
-})
+
+   for (i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener('click', menuClickHandler);
+    };
+
+   burgerButton.addEventListener('click', menuClickHandler);
+   main.addEventListener('click', otherClickHandler);
+   footer.addEventListener('click', otherClickHandler);
+
+   function menuClickHandler(e) {
+        burgerButton.classList.toggle('active');
+        navList.classList.toggle('active');
+        burgerBars.classList.toggle('active');
+    };
+
+   function otherClickHandler(e) {
+        burgerButton.classList.remove('active');
+        navList.classList.remove('active');
+        burgerBars.classList.remove('active');
+    };
+
+};
